1562781174e5ee4ceeeecc7d8c17ae4cc284b632
Libby <129799146+librill@users.noreply.github.com>
2024-11-01 14:00:11 -0700

    debugged

--- src/api/carts.py
+++ src/api/carts.py
@@ -13,17 +13,16 @@ def create_cart(cart_id: int, customer_id: int,payment_id: int):
     with db.engine.begin() as connection:
       cart_id = connection.execute(sqlalchemy.text(
          """
-        INSERT INTO carts(cart_id, customer_id, payment_id)
-        VALUES (:cart_id, :customer_id, :payment_id)
+        INSERT INTO carts(customer_id, payment_id)
+        VALUES (:customer_id, :payment_id)
         RETURNING cart_id
-        """),[{"cart_id": cart_id,
-              "customer_id": customer_id,
+        """),[{"customer_id": customer_id,
               "payment_id": payment_id}]).scalarone()
 
     return {"cart_id":cart_id}
 
 @router.post("/{cart_id}/items/{item_id}")
-def set_item_quantity(cart_id:int, item_id: int,quantity:int):
+def set_item_quantity(cart_id:int, item_id: int, quantity:int):
    """ add items to cart """
    with db.engine.begin() as connection:
       connection.execute(sqlalchemy.text(
@@ -47,18 +46,21 @@ def checkout(cart_id:int,payment_id:int,card_num:int,exp_date:str,customer_id:in
         INSERT INTO payments (payment_id, card_num, exp_date, cvv, customer_id)
         VALUES (:payment_id, :card_num, :exp_date, :cvv, :customer_id)
         RETURNING payment_id, card_num, exp_date, cvv, customer_id
-
-        """))
+        """), [{"payment_id": payment_id,
+                "card_num": card_num,
+                "exp_date": exp_date,
+                "customer_id": customer_id}])
       if not payment:
         raise Exception("Payment failed")
+      
       cart_checkout = connection.execute(sqlalchemy.text(
            """
-        SELECT cart_items.cart_id,
-        cart_items.item_id,
+        SELECT cart_items.cart_id, cart_items.item_id,
         SUM(cart_items.quantity) AS total_ingredients_purchased,
         SUM(cart_items.quantity*ingredients.price) AS total_amount_paid
         FROM cart_items
         JOIN ingredients ON ingredients.ingredient_id = cart_items.item_id
+        WHERE cart_items.cart_id = :cart_id
             """),{"cart_id":cart_id}).scalarone()
       
       total_ingredients_purchased = cart_checkout["total_ingredients_purchased"]
